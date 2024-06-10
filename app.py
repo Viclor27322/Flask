74666816559d58967886e2f893f8b8243e543315
Viclor27322 <vito27322@gmail.com>
2024-06-10 12:13:03 -0600

    Tarea usando modelo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('model.pkl')
+model = joblib.load('modeloDrugs.pyl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
@@ -20,19 +20,26 @@ def home():
 def predict():
     try:
         # Obtener los datos enviados en el request
-        abdomen = float(request.form['abdomen'])
-        antena = float(request.form['antena'])
-        
+        bp = float(request.form['bp'])
+        Na_to_K = float(request.form['Na_to_K'])
+        Cholesterol_HIGH = float(request.form['Cholesterol_HIGH'])
+
         # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        data_df = pd.DataFrame([[bp,Na_to_K,Cholesterol_HIGH]], columns=['BP', 'Na_to_K','Cholesterol_HIGH'])
         app.logger.debug(f'DataFrame creado: {data_df}')
         
         # Realizar predicciones
         prediction = model.predict(data_df)
         app.logger.debug(f'Predicción: {prediction[0]}')
         
+        respuesta = ""
+        if(prediction[0] == 0): respuesta = "DrugY" 
+        if(prediction[0] == 1): respuesta = "drugA" 
+        if(prediction[0] == 2): respuesta = "drugB" 
+        if(prediction[0] == 3): respuesta = "drugC"
+        if(prediction[0] == 4): respuesta = "drugX" 
         # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
+        return jsonify({'categoria': respuesta})
     except Exception as e:
         app.logger.error(f'Error en la predicción: {str(e)}')
         return jsonify({'error': str(e)}), 400
